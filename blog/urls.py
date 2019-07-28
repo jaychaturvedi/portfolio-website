cc06c5ee9c3df85554482dd773c82c347ae20176
jaychaturvedi <jaychaturvedi18@gmail.com>
2019-07-29 01:57:29 +0530

    Styling template

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home, name="home"),
+    path('',views.home, name="bloghome"),
     path("<int:blog_id>/",views.detail,name="detail")
 ]
 
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,7 +6,7 @@ import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',jobs.views.home, name="home"),
+    path('',jobs.views.home, name="jobhome"),
     path('blog/',include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
